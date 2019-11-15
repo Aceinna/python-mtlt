ee5a55dcfa52d32bf43d8f0920b3956d060ea0bd
Tejas Pashte <tejas.pashte@gmail.com>
2019-11-15 14:57:38 -0800

    added custom gps packets

--- CAN_MTLT.py
+++ CAN_MTLT.py
@@ -18,6 +18,11 @@ import binascii
 import gps
 import struct
 import requests
+import csv
+import datetime
+import gnsscal
+
+
 if sys.version_info[0] > 2:
     from queue import Queue
 else:
@@ -25,33 +30,30 @@ else:
 
 #j1939 with extended id
 class can_mtlt:
-    def __init__(self, chn = 'can0', bus_type = 'socketcan_ctypes'):
+    def __init__(self, chn = 'can0', bus_type = 'socketcan'):
         os.system('sudo /sbin/ip link set can0 up type can bitrate 250000')  # run this command first with correct baud rate
         self.can0 = can.interface.Bus(channel = chn, bustype = bus_type)  # socketcan_native
+        self.rows = []
+        self.writer = ""
+
+        fields = ["timeITOW","time","roll","pitch","xRate","yRate","zRate","xAccel","yAccel","zAccel"]
         with open('can_data.txt', 'w') as f:  # empty the can_data.txt
-            print("")
+            f.write("time,roll,pitch,xRate,yRate,zRate,xAccel,yAccel,zAccel"+ "\n")
+            print('')
+
+        # with open('file.csv','w',newline='') as csvfile:
+        #     writer = csv.DictWriter(csvfile, fieldnames = fields)
+        #     writer.writeheader()
+
+            # for row in reader:
+            #     self.rows.append(row)
+
         self.msg_queue = Queue()
         self.fw_version_msg_queue = Queue()
         self.id_msg_queue = Queue()
         self.hw_bit_msg_queue = Queue()
         self.sw_bit_msg_queue = Queue()
         self.sensor_status_msg_queue = Queue()
-
-        # self.gps_direction = {
-        #     "compassBearing":0,
-        #     "navSpeed":0,
-        #     "pitch":0,
-        #     "altitude":0
-        # }
-        # self.gps_wheel_speed = {
-        #     "frontAxleSpeed":0,
-        #     "relSpeedFrontAxleLeftWheel":0,
-        #     "relSpeedFrontAxleRightWheel":0,
-        #     "relSpeedRearAxle1LeftWheel":0,
-        #     "relSpeedRearAxle1RigthWheel":0,
-        #     "relSpeedRearAxle2LeftWheel":0,
-        #     "relSpeedRearAxle2RigthWheel":0
-        # }
         self.pdu_dict = {
             "time_stamp":0,
             "id":0x0000,
@@ -70,14 +72,22 @@ class can_mtlt:
         pitch = pitch_uint * (1/32768) - 250.0
         roll = roll_uint * (1/32768) - 250.0
 
-        #to be done
-        # self.gps_direction["pitch"] = pitch
-        # self.gps_direction["altitude"] = roll
-        # still need to figure out how to get compass bearing and navspeed
+
 
         print('Time: {2:18.6f} Roll: {0:6.2f} Pitch: {1:6.2f}'.format(roll,pitch,msg.timestamp))
         with open('can_data.txt', 'a') as f:
-            f.write('Roll: {0:6.2f} Pitch: {1:6.2f}'.format(roll,pitch) + "\n")
+            # f.write('Roll: {0:6.2f} Pitch: {1:6.2f}'.format(roll,pitch) + "\n")
+            f.write('{0:6.2f},{1:6.2f},{2:6.2f},0,0,0,0,0,0'.format(msg.timestamp,roll,pitch) + "\n")
+
+        # with open('file.csv','r') as csvfile:
+        #     reader = csv.reader(csvfile)
+        #     lines = list(reader)
+        #     lines.append([msg.timestamp,msg.timestamp,roll,pitch,0,0,0,0,0,0])
+        #
+        # with open('file.csv','w') as file:
+        #     writer = csv.writer(file)
+        #     writer.writerows(lines)
+
     def print_accel(self,msg):   # unit: g
         ax_uint = msg.data[0] + 256 * msg.data[1]
         ay_uint = msg.data[2] + 256 * msg.data[3]
@@ -85,9 +95,22 @@ class can_mtlt:
         ax = ax_uint * (0.01) - 320.0
         ay = ay_uint * (0.01) - 320.0
         az = az_uint * (0.01) - 320.0
+
+        # date = time.ctime(msg.timestamp)
         print('Time: {3:18.6f} AX  : {0:6.2f} AY   : {1:6.2f} AZ: {2:6.2f}'.format(ax,ay,az,msg.timestamp))
         with open('can_data.txt', 'a') as f:
-            f.write('AX  : {0:6.2f} AY   : {1:6.2f} AZ: {2:6.2f}'.format(ax,ay,az) + "\n")
+            # f.write('AX  : {0:6.2f} AY   : {1:6.2f} AZ: {2:6.2f}'.format(ax,ay,az) + "\n")
+            f.write('{0:6.2f},0,0,0,0,0,{1:6.2f},{2:6.2f},{3:6.2f}'.format(msg.timestamp,ax,ay,az) + "\n")
+
+        # with open('file.csv','r') as csvfile:
+        #     reader = csv.reader(csvfile)
+        #     lines = list(reader)
+        #     lines.append([msg.timestamp,msg.timestamp,0,0,0,0,0,ax,ay,az])
+        #
+        # with open('file.csv','w') as file:
+        #     writer = csv.writer(file)
+        #     writer.writerows(lines)
+
     def print_rate(self,msg):    # degree per second
         wx_uint = msg.data[0] + 256 * msg.data[1]
         wy_uint = msg.data[2] + 256 * msg.data[3]
@@ -95,9 +118,21 @@ class can_mtlt:
         wx = wx_uint * (1/128.0) - 250.0
         wy = wy_uint * (1/128.0) - 250.0
         wz = wz_uint * (1/128.0) - 250.0
+
+
         print('Time: {3:18.6f} WX  : {0:6.2f} WY   : {1:6.2f} WZ: {2:6.2f}'.format(wx,wy,wz,msg.timestamp))
         with open('can_data.txt', 'a') as f:
-            f.write('WX  : {0:6.2f} WY   : {1:6.2f} WZ: {2:6.2f}'.format(wx,wy,wz) + "\n")
+            # f.write('WX  : {0:6.2f} WY   : {1:6.2f} WZ: {2:6.2f}'.format(wx,wy,wz) + "\n")
+            f.write('{0:6.2f},0,0,{1:6.2f},{2:6.2f},{3:6.2f},0,0,0'.format(msg.timestamp,wx,wy,wz) + "\n")
+
+        # with open('file.csv','r') as csvfile:
+        #     reader = csv.reader(csvfile)
+        #     lines = list(reader)
+        #     lines.append([msg.timestamp,msg.timestamp,0,0,wx,wy,wz,0,0,0])
+        #
+        # with open('file.csv','w') as file:
+        #     writer = csv.writer(file)
+        #     writer.writerows(lines)
 
  #to be finished
     # def gps_position(self,msg):
@@ -121,49 +156,51 @@ class can_mtlt:
                 msg_read = self.can0.recv()
 
             self.get_pdu_list(msg = msg_read)
+            print(self.pdu_dict["pgn"])
             #print to screen and can_data.txt file
-            if self.pdu_dict["pgn"] == 65267:
-                with open('can_data.txt', 'a') as f:  #store the raw message to file
-                    f.write(msg_read.__str__() + "  ")
-                self.print_gps(msg_read)
-                print('gps detected')
-            elif self.pdu_dict["pgn"] == 61481:
-                with open('can_data.txt', 'a') as f:  #store the raw message to file
-                    f.write(msg_read.__str__() + "  ")
-                self.print_slope(msg_read)
-            elif self.pdu_dict["pgn"] == 61482:
-                with open('can_data.txt', 'a') as f:  #store the raw message to file
-                    f.write(msg_read.__str__() + "  ")
-                self.print_rate(msg_read)
-                # pass
-            elif self.pdu_dict["pgn"] == 61485:
-                with open('can_data.txt', 'a') as f:  #store the raw message to file
-                    f.write(msg_read.__str__() + "  ")
-                self.print_accel(msg_read)
-                # pass
-            elif self.pdu_dict["pgn"] == 61183:
-                # pass
-                print("Time:", self.pdu_dict["time_stamp"], self.pdu_dict["pgn"], self.pdu_dict["payload"], "------Address_Claiming")     #only print to screen, no write to file
-            elif self.pdu_dict["pgn"] == 65242:
-                self.fw_version_msg_queue.put(self.pdu_dict)
-                print(self.pdu_dict)
-            elif self.pdu_dict["pgn"] == 64965:
-                self.id_msg_queue.put(self.pdu_dict)
-                print(self.pdu_dict)
-            elif self.pdu_dict["pgn"] == 65362:
-                self.hw_bit_msg_queue.put(self.pdu_dict)
-                print(self.pdu_dict)
-            elif self.pdu_dict["pgn"] == 65362:
-                self.hw_bit_msg_queue.put(self.pdu_dict)
-                print(self.pdu_dict)
-            elif self.pdu_dict["pgn"] == 65363:
-                self.sw_bit_msg_queue.put(self.pdu_dict)
-                print(self.pdu_dict)
-            elif self.pdu_dict["pgn"] == 65364:
-                self.sensor_status_msg_queue.put(self.pdu_dict)
-                print(self.pdu_dict)
-            else:
-                print(msg_read, "\n")
+            # if self.pdu_dict["pgn"] == 65267:
+            #     # with open('can_data.txt', 'a') as f:  #store the raw message to file
+            #         # f.write(msg_read.__str__() + "  ")
+            #
+            #     self.print_gps(msg_read)
+            #     print('gps detected')
+            # elif self.pdu_dict["pgn"] == 61481:
+            #     # with open('can_data.txt', 'a') as f:  #store the raw message to file
+            #         # f.write(msg_read.__str__() + "  ")
+            #     self.print_slope(msg_read)
+            # elif self.pdu_dict["pgn"] == 61482:
+            #     # with open('can_data.txt', 'a') as f:  #store the raw message to file
+            #         # f.write(msg_read.__str__() + "  ")
+            #     self.print_rate(msg_read)
+            #     # pass
+            # elif self.pdu_dict["pgn"] == 61485:
+            #     # with open('can_data.txt', 'a') as f:  #store the raw message to file
+            #         # f.write(msg_read.__str__() + "  ")
+            #     self.print_accel(msg_read)
+            #     # pass
+            # elif self.pdu_dict["pgn"] == 61183:
+            #     # pass
+            #     print("Time:", self.pdu_dict["time_stamp"], self.pdu_dict["pgn"], self.pdu_dict["payload"], "------Address_Claiming")     #only print to screen, no write to file
+            # elif self.pdu_dict["pgn"] == 65242:
+            #     self.fw_version_msg_queue.put(self.pdu_dict)
+            #     print(self.pdu_dict)
+            # elif self.pdu_dict["pgn"] == 64965:
+            #     self.id_msg_queue.put(self.pdu_dict)
+            #     print(self.pdu_dict)
+            # elif self.pdu_dict["pgn"] == 65362:
+            #     self.hw_bit_msg_queue.put(self.pdu_dict)
+            #     print(self.pdu_dict)
+            # elif self.pdu_dict["pgn"] == 65362:
+            #     self.hw_bit_msg_queue.put(self.pdu_dict)
+            #     print(self.pdu_dict)
+            # elif self.pdu_dict["pgn"] == 65363:
+            #     self.sw_bit_msg_queue.put(self.pdu_dict)
+            #     print(self.pdu_dict)
+            # elif self.pdu_dict["pgn"] == 65364:
+            #     self.sensor_status_msg_queue.put(self.pdu_dict)
+            #     print(self.pdu_dict)
+            # else:
+            #     print(msg_read, "\n")
 
             #to be finished
             # if self.gps_position["lat"] > 0:
@@ -295,13 +332,11 @@ class can_mtlt:
         OFFSET = 1 << 32
         MASK = OFFSET - 1
         hex = '%08x' % (num + OFFSET & MASK)
-        print(hex)
         bytes = []
 
         for i in range(n, 4):
             temp = '0x' + hex[i * 2: i * 2 + 2]
             bytes.insert(0,int(temp,16))
-
         return bytes[::]
 
     def read_gps(self):
@@ -312,57 +347,99 @@ class can_mtlt:
             rep = session.next()
 
             try :
-                # 64502
-                if (rep["class"] == "SKY"):
-                    hDop = int(rep.hdop * 10)
-                    vDop = int(rep.vdop * 10)
-                    pDop = int(rep.pdop * 10)
-                    tDop = int(rep.tdop * 10)
-                    rsvd1 = 0
-                    rsvd2 = 0
-                    rsdv3= 0
-                    sat_num = len(rep.satellites)
-
-                    dilution_val = [sat_num,hDop,vDop,pDop,tDop]
-                    print(dilution_val)
-                    my_can.send_msg(0x18FBF680,dilution_val)
-
-
+                sat_num = 0
+                #params for 64502 packet
                 if (rep["class"] == "TPV") :
+                    # if sat_num > 0 :
+
+                    #params for 65390
+                    timestamp = int(datetime.datetime.now().timestamp() * 1000)
+                    ground_speed = int(rep.speed)
+                    strTimestamp = self.int2Hex(timestamp,0)
+                    strSpeed = self.int2Hex(ground_speed,0)
+
                     # params for 65257 packet
                     lat = int((rep.lat + 210) * 1000000)
                     lon = int((rep.lon + 210) * 1000000)
-
-                    #params for 65256 packet
-                    #conversions according to j1939 excel
-                    alt = int((rep.alt + 2500) / 0.125)
-                    pitch = 0
-                    speed = int(rep.speed * 256)
-                    heading = int(rep.track * 128)
-
-                    strHeading = self.int2Hex(heading,2)
-                    strSpeed = self.int2Hex(speed,2)
-                    strPitch = self.int2Hex(pitch,2)
-                    strAlt = self.int2Hex(alt,2)
-
-                    vehicle_pos = strHeading + strSpeed + strPitch + strAlt
-
                     strLat = self.int2Hex(lat,0)
                     strLong = self.int2Hex(lon,0)
-                    data = strLong + strLat
-                    vehicle_gps = strLat + strLong
 
-                    print(vehicle_gps)
-                    print(vehicle_pos)
+                    # params for 65391
+                    mode = rep.mode
+                    alt = int(rep.alt * 1000)
+                    strAlt = self.int2Hex(alt,0)
+
+                    #params for 65392
+                    eph = int(rep.eph) * 1000
+                    strEph = self.int2Hex(eph,2)
+
+                    epv = int(rep.epv) * 1000
+                    strEpv = self.int2Hex(epv,2)
+
+                    heading = (int(rep.track) * 100000)/182
+                    strHeading = self.int2Hex(heading,2)
+
+                    vehicle_gps = strLat + strLong
+                    vehicle_speed = strTimestamp + strSpeed
+
                     my_can.send_msg(0x18FEF380,vehicle_gps)
-                    my_can.send_msg(0x18FEE880,vehicle_pos)
+                    my_can.send_msg(0x18FF6E80,vehicle_speed)
+
+
+                    ######### J1939 format: available for referrence ######
+
+                    # #params for 65256 packet
+                    # #conversions according to j1939 excel
+                    # alt = int((rep.alt + 2500) / 0.125)
+                    # pitch = 0
+                    # speed = int(rep.speed * 256)
+                    # heading = int(rep.track * 128)
+                    #
+                    # strHeading = self.int2Hex(heading,2)
+                    # strSpeed = self.int2Hex(speed,2)
+                    # strPitch = self.int2Hex(pitch,2)
+                    # strAlt = self.int2Hex(alt,2)
+                    #
+                    # vehicle_pos = strHeading + strSpeed + strPitch + strAlt
+                    #
+
+                    # data = strLong + strLat
+
+                    #     hDop = int(rep.hdop * 10)
+                    #     vDop = int(rep.vdop * 10)
+                    #     pDop = int(rep.pdop * 10)
+                    #     tDop = int(rep.tdop * 10)
+                    #     rsvd1 = 0
+                    #     rsvd2 = 0
+                    #     rsdv3= 0
+                    #     sat_num = len(rep.satellites)
+                    #
+                    #     dilution_val = [sat_num,hDop,vDop,pDop,tDop]
+                    #     print("pgn 64502",dilution_val)
+                    #
+                    #        my_can.send_msg(0x18FBF680,dilution_val)
+
+                if (rep["class"] == "SKY"):
+                    # params for 65391
+                    sat_num = len(rep.satellites)
+                    flags = 1
+                    valid = 1
+                    packet_3 =  sat_num + flags + valid + mode + strAlt
+
+                    #params for 65392
+                    pDop = int(rep.pdop * 10)
+                    strPdop = self.int2Hex(heading,2)
+                    packet_2 = strEph + strEpv + strHeading + strPdop
+
+                    my_can.send_msg(0x18FF7080,packet_2)
+                    my_can.send_msg(0x18FF6F80,packet_3)
 
             except Exception as e :
                 print("Got exception " + str(e))
 
 
 if __name__ == "__main__":
-    my_can = can_mtlt('can0', 'socketcan_ctypes')
+    my_can = can_mtlt('can0', 'socketcan')
 
     # get messages
     # my_can.get_fw_version()
